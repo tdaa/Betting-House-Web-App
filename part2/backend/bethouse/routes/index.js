1ece069f0adfcb6a507b18688f8afa2da9267457
MGCSousa <manuelgcsousa@gmail.com>
2019-04-28 20:59:44 +0100

    Adição res.jsonp

--- part2/backend/bethouse/routes/index.js
+++ part2/backend/bethouse/routes/index.js
@@ -5,8 +5,11 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
     Categorias.listar()
-        .then(dados => console.log(dados))
-        .catch(err => console.log(err));
+        .then(dados => {
+            console.log(dados);
+            res.jsonp(dados);
+        })
+        .catch(err => res.status(500).send('Erro na listagem: ' + err));
 });
 
 module.exports = router;
